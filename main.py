6281d18d87e6aa33180b80f23747fc9857ae57b1
franekl <franek.liszka@outlook.com>
2022-09-05 21:24:50 +0200

    updated main.py

--- main.py
+++ main.py
@@ -92,20 +92,22 @@ max_date = datetime.datetime.strptime(max_date, '%Y-%m-%d')
 
 
 
+try:
+    uploaded_files = st.file_uploader("Upload xlsx files below", type="xlsx", accept_multiple_files=True)
 
-uploaded_files = st.file_uploader("Upload xlsx files below", type="xlsx", accept_multiple_files=True)
-
-if uploaded_files:
-    x = []
-    for file in uploaded_files:
-        file.seek(0)
-            
-    uploaded_data_read = [pd.read_excel(file, header=None) for file in uploaded_files]
-    raw_data = pd.concat(uploaded_data_read)
-    raw_data = raw_data[raw_data[0]=="Date"]
-    raw_data[1] = pd.to_datetime(raw_data[1], format='%d.%m.%Y %H:%M:%S')
-    min_date = raw_data[1].min()
-    max_date = raw_data[1].max()
+    if uploaded_files:
+        x = []
+        for file in uploaded_files:
+            file.seek(0)
+                
+        uploaded_data_read = [pd.read_excel(file, header=None) for file in uploaded_files]
+        raw_data = pd.concat(uploaded_data_read)
+        raw_data = raw_data[raw_data[0]=="Date"]
+        raw_data[1] = pd.to_datetime(raw_data[1], format='%d.%m.%Y %H:%M:%S')
+        min_date = raw_data[1].min()
+        max_date = raw_data[1].max()
+except:
+    pass
 
 
 
