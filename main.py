ee4cf344c85771996689cbdbff006c782be48942
franekl <franek.liszka@outlook.com>
2022-09-28 09:10:03 +0200

    added try-except in the main script

--- main.py
+++ main.py
@@ -151,22 +151,23 @@ with st.form(key='my_form'):
 # end_datetime = datetime.strptime(end_date, '%Y-%m-%d %H:%M:%S')
 
 
+try:
+    # %%
+    start_date = datetime.datetime.combine(date1,t1)
+    # end_date = datetime.datetime.combine(date2,t2)
+    end_date = '2025-08-08 12:00:00'
 
+    df = preprocess(uploaded_files=uploaded_files, start_date=start_date, end_date=end_date)
+    st.dataframe(df)
+    csv = convert_df(df)
 
-# %%
-start_date = datetime.datetime.combine(date1,t1)
-# end_date = datetime.datetime.combine(date2,t2)
-end_date = '2025-08-08 12:00:00'
-
-df = preprocess(uploaded_files=uploaded_files, start_date=start_date, end_date=end_date)
-st.dataframe(df)
-csv = convert_df(df)
-
-st.download_button(
-"Press to Download",
-csv,
-output_name + ".csv",
-"text/csv",
-key='download-csv'
-)
+    st.download_button(
+    "Press to Download",
+    csv,
+    output_name + ".csv",
+    "text/csv",
+    key='download-csv'
+    )
+except:
+    pass
 
