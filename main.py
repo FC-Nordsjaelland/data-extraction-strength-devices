e224fc80b7a62d5edca4fe55a55ac759f33e9edb
franekl <franek.liszka@outlook.com>
2022-09-28 10:53:36 +0200

    made changes in main

--- main.py
+++ main.py
@@ -88,23 +88,6 @@ max_date = datetime.datetime.strptime(max_date, '%Y-%m-%d')
 
 
 
-
-uploaded_files = st.file_uploader("Upload xlsx files below", type="xlsx", accept_multiple_files=True)
-
-if len(uploaded_files) > 3:
-    for file in uploaded_files:
-        file.seek(0)
-    try:
-        uploaded_data_read = [pd.read_excel(file, header=None) for file in uploaded_files]
-        raw_data = pd.concat(uploaded_data_read)
-        raw_data = raw_data[raw_data[0]=="Date"]
-        raw_data[1] = pd.to_datetime(raw_data[1], format='%d.%m.%Y %H:%M:%S')
-        min_date = raw_data[1].min()
-        max_date = raw_data[1].max()
-    except:
-        pass
-
-
 with st.form(key='my_form'):
 
     c1, c2 = st.columns(2)
@@ -127,6 +110,24 @@ with st.form(key='my_form'):
 
     output_name = st.text_input("Enter the output file name", "output")
     st.form_submit_button()
+
+
+
+uploaded_files = st.file_uploader("Upload xlsx files below", type="xlsx", accept_multiple_files=True)
+
+if len(uploaded_files) > 3:
+    for file in uploaded_files:
+        file.seek(0)
+    try:
+        uploaded_data_read = [pd.read_excel(file, header=None) for file in uploaded_files]
+        raw_data = pd.concat(uploaded_data_read)
+        raw_data = raw_data[raw_data[0]=="Date"]
+        raw_data[1] = pd.to_datetime(raw_data[1], format='%d.%m.%Y %H:%M:%S')
+        min_date = raw_data[1].min()
+        max_date = raw_data[1].max()
+    except:
+        pass
+
 
 
 
