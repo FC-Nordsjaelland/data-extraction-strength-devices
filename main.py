6a6ad54f81fd004f5b08469498e23020fdc1f0c8
franekl <franek.liszka@outlook.com>
2022-09-28 10:50:59 +0200

    made changes in main

--- main.py
+++ main.py
@@ -153,6 +153,7 @@ start_date = datetime.datetime.combine(date1,t1)
 end_date = '2025-08-08 12:00:00'
 
 try:
+    st.dataframe(raw_data)
     df = preprocess(uploaded_files=uploaded_files, start_date=start_date, end_date=end_date)
     st.dataframe(df)
     csv = convert_df(df)
