c4a473bdddb01be47e97b729e70f0582961cf7f3
franekl <franek.liszka@outlook.com>
2022-10-04 17:32:10 +0200

    changes in pages

--- pages/page2_viz.py
+++ pages/page2_viz.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.set_page_config(page_title="Strength data visualization", page_icon="☀️", layout="wide")
+# st.set_page_config(page_title="Strength data visualization", page_icon="☀️", layout="wide")
 st.sidebar.markdown("## Strength data visualization")
