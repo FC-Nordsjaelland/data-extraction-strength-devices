0c9efb2646619be4f74375bd8c786a5dc29c3e2b
franekl <franek.liszka@outlook.com>
2022-10-04 17:15:05 +0200

    testing the excel feature

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #%%
-from pickle import FALSE
 import pandas as pd
 import datetime
 from datetime import time
@@ -46,9 +45,9 @@ def preprocess(uploaded_files, start_date, end_date):
     # file_names = glob.glob(os.path.join(dir_path, "*.xlsx"))
 
     players_data = []
-    # for file in uploaded_files:
-    one_player = flatten_xlsx(uploaded_files)
-    players_data.append(one_player)
+    for file in uploaded_files:
+        one_player = flatten_xlsx(file)
+        players_data.append(one_player)
         #line to add another column or so 
 
     
@@ -91,27 +90,21 @@ max_date = datetime.datetime.strptime(max_date, '%Y-%m-%d')
 
 
     
-uploaded_files = st.file_uploader("Upload xlsx files below", type="xlsx", accept_multiple_files=False)
-try:
-    raw_data = pd.read_excel(uploaded_files)
-    raw_data = raw_data[raw_data[0]=="Date"]
-    raw_data[1] = pd.to_datetime(raw_data[1], format='%d.%m.%Y %H:%M:%S')
-except:
-    pass
-
-# if uploaded_files is not None:
-#     uploaded_data_read = []
+uploaded_files = st.file_uploader("Upload xlsx files below", type="xlsx", accept_multiple_files=True)
     
-#     try:
-#         for file in uploaded_files:
-#             uploaded_data_read = [pd.read_excel(file, header=None) for file in uploaded_files]
-#             raw_data = pd.concat(uploaded_data_read)
-#             raw_data = raw_data[raw_data[0]=="Date"]
-#             raw_data[1] = pd.to_datetime(raw_data[1], format='%d.%m.%Y %H:%M:%S')
-#             min_date = raw_data[1].min()
-#             max_date = raw_data[1].max()
-#     except:
-#         pass
+if uploaded_files is not None:
+    uploaded_data_read = []
+    
+    try:
+        for file in uploaded_files:
+            uploaded_data_read = [pd.read_excel(file, header=None) for file in uploaded_files]
+            raw_data = pd.concat(uploaded_data_read)
+            raw_data = raw_data[raw_data[0]=="Date"]
+            raw_data[1] = pd.to_datetime(raw_data[1], format='%d.%m.%Y %H:%M:%S')
+            min_date = raw_data[1].min()
+            max_date = raw_data[1].max()
+    except:
+        pass
 
 
 
