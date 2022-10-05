5a6e04cbe92dbe54ee8bb9dc79590ae81f8de9db
franekl <franek.liszka@outlook.com>
2022-10-05 09:19:09 +0200

    updated markdown in the sidebar (instructions)"

--- Data_Extraction.py
+++ Data_Extraction.py
@@ -11,7 +11,7 @@ from st_aggrid import AgGrid
 st.set_page_config(page_title="Strength data summary", layout ='wide')
 st.sidebar.markdown("## Strength data summary")
 st.sidebar.header("**Instruction**")
-st.sidebar.markdown("1) Right click, copy and open the [link](%s)" % link, unsafe_allow_html=True)
+st.sidebar.markdown("1) Right click, copy and open the [link]")
 st.sidebar.markdown("2) After a successful authentication, expand the link of the page and copy the code (fx. 'http://xyz/?code=u9xMkv')")
 st.sidebar.markdown("3) Paste the code into the first cell in the form below")
 st.sidebar.markdown("4) Choose your team, training/match date, session number (in case there's multiple sessions in a day, fx. 1 stands for the first session in a day")  
