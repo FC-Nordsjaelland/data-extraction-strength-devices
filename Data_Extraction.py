8dad88b81f99b14699f7ad2868d051201bf5ad1b
franekl <franek.liszka@outlook.com>
2022-10-06 19:10:59 +0200

    adding the perc diff functionality

--- Data_Extraction.py
+++ Data_Extraction.py
@@ -94,7 +94,7 @@ def preprocess(uploaded_files, start_date, end_date):
     final_df = final_df.reset_index(drop=True)
 
     final_df['Percentage difference'] = percentage_difference(final_df['Max left'], final_df['Max right'])
-    # final_df['Percentage difference'] = final_df['Percentage difference'].round(decimals=1)
+    final_df['Percentage difference'] = final_df['Percentage difference'].round(decimals=1)
     final_df = final_df[['Date','Team','Name', 'Device','Max left', 'Max right', 'Percentage difference', 'Comment']]
     return final_df
 
